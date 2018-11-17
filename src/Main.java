608628b5958aac4332d8d171478f2c1349e42c71
Anna Mareczko <an.mareczko@gmail.com>
2018-11-17 15:03:40 +0100

    feature hellow 12323654

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Logger.log("Hello 125");
-        Logger.log("Hello hellow");
+        Logger.log("Hello 125654654");
+
     }
 }
